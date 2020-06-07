9aec035966bfb5226e77e96628f9fe80ae697b06
Laser Yuan <laser.yuan@gmail.com>
2020-06-07 14:27:00 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+```
+cd ~/projects/kodi/context.item.savebookmarks
+
+docker run -it --rm --name=kodi \
+  --privileged \
+  -e DISPLAY=unix:0 -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e PULSE_SERVER=unix:/run/user/1000/pulse/native -v /run/user/1000:/run/user/1000 \
+  -v /var/run/dbus/:/var/run/dbus/ \
+  -v /home/laser/Videos/:/home/kodi/Videos/ `# mount video library`\
+  -v kodi-config:/home/kodi/.kodi/userdata `# persist kodi configuration`\
+  -v kodi-addons:/home/kodi/.kodi/addons/ `# persist kodi addon dependencies`\
+  -v $(pwd)/:/home/kodi/.kodi/addons/context.item.savebookmarks/ `# mount the addon; needs to be enabled in kodi config`\
+  lasery/codebench:kodi \
+  bash
+
+cd ~/.kodi/addons/context.item.savebookmarks/
+python fork/tests.py
+
+cat /home/kodi/.kodi/temp/kodi.log
+```
+
---
+++ addon.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import re
+from sqlite3 import dbapi2 as database
+
+import xbmc
+import xbmcgui
+import xbmcvfs
+
+def make_filename(title):
+    filename = title.replace("|", "_").replace("?", "_").replace("/", "_")
+    return filename
+
+def save_file_to_folder(path, data):
+    file = path + '/' + make_filename(sys.listitem.getLabel()) + '.bmk'
+    f = xbmcvfs.File (file, 'w')
+    result = f.write(str(data))
+    f.close()
+
+def main():
+    positions = get_posts_from_bookmark()
+    xbmcgui.Dialog().ok("positions", str(positions))
+    xbmc.log( "context.item.savebookmarks: bookmark positions: %s" % str(positions), xbmc.LOGNOTICE )
+
+    dialog = xbmcgui.Dialog()
+    path = dialog.browseSingle(0, 'Select directory for save .bmk', 'video')
+
+    if path != "":
+        xbmc.log( "context.item.savebookmarks: save to: %s" % path, xbmc.LOGNOTICE )
+        save_file_to_folder(path, positions)
+
+def getDbFile():
+    db_path = xbmc.translatePath('special://database').decode('utf-8')
+    return os.path.join(db_path, 'MyVideos107.db')
+
+def translateItemPath(itemPath):
+    ret = itemPath
+    if "PlayMedia" in itemPath:
+        ret = re.findall('"([^"]*)"', ret).pop()
+    return ret
+
+def get_posts_from_bookmark():
+    dbcon = database.connect(getDbFile())
+    dbcon.row_factory = lambda cursor, row: row[0]
+    dbcur =  dbcon.cursor()
+
+    itemPath = sys.listitem.getPath()
+    filePath = translateItemPath(itemPath)
+    dirPath = os.path.dirname(filePath) + os.sep
+    ret = dbcur.execute('SELECT timeInSeconds FROM main.bookmark WHERE idFile=(SELECT idFile FROM main.files WHERE idPath=(SELECT idPath FROM main.path WHERE strPath=?) AND strFilename=?) ORDER BY timeInSeconds',(dirPath, os.path.basename(filePath),)).fetchall()
+    dbcon.close()
+    return ret
+
+if __name__ == '__main__':
+    main()
